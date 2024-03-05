3e23a714d7dc87e43540a3201be8af0ccad7e24f
Kevin <150013294+KevinHogendoorn@users.noreply.github.com>
2024-03-05 13:49:38 +0100

    Add files via upload
    Ejercicio acabado con todos los test

--- PracticaEvaluable/src/es/iesmz/EmpleadoBRTest.java
+++ PracticaEvaluable/src/es/iesmz/EmpleadoBRTest.java
@@ -1,5 +1,6 @@
 package es.iesmz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class EmpleadoBRTest {
     @Test
     public void testCalcularSalarioBruto1(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(TipoEmpleado.vendedor, 2000.0f, 8.0f);
         float resultadoEsperado = 1360.0f;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -14,6 +16,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioNeto1(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioNeto(2000);
         float resultadoEsperado = 1640;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -21,6 +24,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioNeto2(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioNeto(1500);
         float resultadoEsperado = 1230;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -33,6 +37,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioNeto3(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioNeto((float) 1499.99);
         float resultadoEsperado = (float) 1259.9916;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -40,6 +45,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioNeto4(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioNeto(1250);
         float resultadoEsperado = 1050;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -47,6 +53,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioNeto5(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioNeto(1000);
         float resultadoEsperado = 840;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -54,6 +61,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioNeto6(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioNeto((float) 999.99);
         float resultadoEsperado = (float) 999.99;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -61,6 +69,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioNeto7(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioNeto(500);
         float resultadoEsperado = 500;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -68,6 +77,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioNeto8(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioNeto(0);
         float resultadoEsperado = 0;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -75,6 +85,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioNeto9(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioNeto(-1);
         float resultadoEsperado = -1;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -82,6 +93,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioBruto1(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(TipoEmpleado.vendedor, 2000, 8);
         float resultadoEsperado = 1360;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -89,6 +101,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioBruto2(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(TipoEmpleado.vendedor, 1500, 3);
         float resultadoEsperado = 1260;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -96,6 +109,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioBruto3(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(TipoEmpleado.vendedor, (float) 1499.99, 0);
         float resultadoEsperado = 1100;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -103,6 +117,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioBruto4(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(TipoEmpleado.encarregat, 1250, 8);
         float resultadoEsperado = 1360;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -110,6 +125,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioBruto5(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(TipoEmpleado.encarregat, 1000, 0);
         float resultadoEsperado = 1600;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -117,6 +133,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioBruto6(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(TipoEmpleado.encarregat, (float) 999.99, 3);
         float resultadoEsperado = 1560;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -124,6 +141,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioBruto7(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(TipoEmpleado.encarregat, 500, 0);
         float resultadoEsperado = 1500;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -131,6 +149,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioBruto8(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(TipoEmpleado.encarregat, 0, 8);
         float resultadoEsperado = 1660;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -138,6 +157,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioBruto9(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(TipoEmpleado.vendedor, -1, 8);
         float resultadoEsperado = -1;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -145,6 +165,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioBruto10(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(TipoEmpleado.vendedor, 1500, 1);
         float resultadoEsperado = -1;
         assertEquals(resultadoEsperado, resultadoReal);
@@ -152,6 +173,7 @@ class EmpleadoBRTest {
 
     @Test
     public void calcularSalarioBruto11(){
+        System.out.println("veure @BeforeEach");
         float resultadoReal = EmpleadoBR.calcularSalarioBruto(null, 1500, 8);
         float resultadoEsperado = -1;
         assertEquals(resultadoEsperado, resultadoReal);
